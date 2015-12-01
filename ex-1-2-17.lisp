c41f9913897565e799edb9cc45a4919d4bb3c7da
wott <william.ott@medicustek.com>
2015-12-01 17:24:17 +0800

    Finish extra credit

--- ex-1-2-17.lisp
+++ ex-1-2-17.lisp
@@ -9,16 +9,26 @@
   (write-byte 101 b)
   (write-byte 121 b))
 
-(defvar *monkey* '(109 111 110 107 101 121))
 
 (with-open-file (b "/tmp/binary-monkey.txt"
                    :direction :output
                    :element-type 'unsigned-byte
                    :if-exists :supersede)
-  (do* ((i 0 (+ i 1)) (c (first *monkey*) (nth i *monkey*))) 
-       ((>= i (length *monkey*)) "done")
-    (write-byte c b)))
+  (let ((monkey '(#x6d #x6f #x6e #x6b #x65 #x79)))
+    (do* ((i 0 (+ i 1)) (c (first monkey) (nth i monkey))) 
+       ((>= i (length monkey)) "done")
+    (write-byte c b))))
 
+(with-open-file (b "/tmp/binary-monkey.txt"
+                   :direction :output
+                   :element-type 'unsigned-byte
+                   :if-exists :supersede)
+  (let ((monkey '(#o155 #o157 #o156 #o153 #o145 #o171)))
+    (do* ((i 0 (+ i 1)) (c (first monkey) (nth i monkey))) 
+       ((>= i (length monkey)) "done")
+    (write-byte c b))))
+
+;;; For case conversion in format: http://www.lispworks.com/documentation/HyperSpec/Body/22_cha.htm
 (with-open-file (s "/tmp/binary-monkey.txt"
                    :direction :input)
-  (format t "" (read-line s)))
+  (format t "~:@(~A~)~%" (read-line s)))
